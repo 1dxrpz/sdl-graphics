4849c458f28799a475c9f88bede820eee6b36d38
1dxrpz <scaphoidederps@gmail.com>
2023-10-07 17:14:08 +0300

    x64/x32

--- Makefile
+++ Makefile
@@ -1,2 +1,22 @@
+COMPILER32=i686-w64-mingw32-gcc
+COMPILER64=x86_64-w64-mingw32-g++
+
+EXE=main
+ENTRY=main
+LIBS32=-I src/x32/include -L src/x32/lib
+LIBS64=-I src/x64/include -L src/x64/lib
+FLAGS=-lSDL2main -lSDL2 -static-libgcc -static-libstdc++
+
+DEBUG=$(ENTRY).cpp $(FLAGS) -o $(EXE)
+
 all:
-	x86_64-w64-mingw32-g++ -I src/include -L src/lib -o main main.cpp -lSDL2main -lSDL2 -static-libgcc -static-libstdc++
+	$(COMPILER64) $(LIBS64) $(DEBUG)_x64
+
+release64:
+	debug64 -mwindows
+
+debug32:
+	$(COMPILER32) $(LIBS32) $(DEBUG)_x32
+
+release32:
+	debug32 -mwindows
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define SDL_MAIN_HANDLED
+
 #include <SDL2/SDL.h>
 
 
@@ -7,12 +8,23 @@ const int WIDTH = 800, HEIGHT = 600;
 
 
 
-int main(int argc, char *argv[])
+int WinMain(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
     
 
     SDL_Window *win = SDL_CreateWindow("Hello world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    SDL_RenderDrawLine(renderer, 100, 100, 300, 300);
+
+    SDL_RenderPresent(renderer);
 
     if (win == NULL)
     {
